42df5d8e8bbd8a6fb7083d223c38968d5e913d1f
Oranore <60654293+Oranore@users.noreply.github.com>
2020-03-19 15:05:11 +0100

    Add bold and fontsize on p about

--- js/scriptAbout.js
+++ js/scriptAbout.js
@@ -10,3 +10,21 @@ function switchTheme(event) {
 const toggler = document.querySelector('#toggler');
 toggler.addEventListener('change', switchTheme, false);
 
+// Texte en gras au survol de la souris
+
+let paraBold = document.querySelector('.para1');
+let para2Bold = document.querySelector('.para2');
+
+paraBold.addEventListener('mouseover', function () { this.style.fontWeight = 'bold' });
+paraBold.addEventListener('mouseout', function () { this.style.fontWeight = 'normal'});
+
+para2Bold.addEventListener('mouseover', function () { this.style.fontWeight = 'bold' });
+para2Bold.addEventListener('mouseout', function () { this.style.fontWeight = 'normal'});
+
+// Font size augmentée au passsage de la souris
+
+paraBold.addEventListener('mouseover', function () { this.style.fontSize = '35px'});
+paraBold.addEventListener('mouseout', function () { this.style.fontSize = '25px'});
+
+para2Bold.addEventListener('mouseover', function () { this.style.fontSize = '35px'});
+para2Bold.addEventListener('mouseout', function () { this.style.fontSize = '25px'});
